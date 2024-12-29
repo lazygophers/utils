b254fb34a379d036f3f15adb0f3a80cd11ad00f2
nico <nico@starpay.com>
2024-12-30 01:19:50 +0800

    feature: cache 添加默认值的方法

--- routine/cache.go
+++ routine/cache.go
@@ -23,12 +23,29 @@ func (p *Cache[K, V]) Get(key K) (V, bool) {
 	v, ok := p.data[key]
 	p.RUnlock()
 
+	if !ok {
+		return *new(V), false
+	}
+
 	if !v.expire.IsZero() && time.Now().Before(v.expire) {
 		p.Delete(key)
 		return v.value, false
 	}
 
 	return v.value, ok
+}
+
+func (p *Cache[K, V]) GetWithDef(key K, def ...V) (V, bool) {
+	value, ok := p.Get(key)
+	if !ok {
+		if len(def) > 0 {
+			return def[0], ok
+		}
+
+		return *new(V), ok
+	}
+
+	return value, ok
 }
 
 func (p *Cache[K, V]) Set(key K, value V) {
