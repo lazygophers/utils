680c7bcd0ff0e4f5d613599fa2c2ad9e5ceb7691
nico <nico@lofreer.com>
2026-03-09 16:40:11 +0800

    refactor(candy): 重构键值映射功能以提高代码复用性
    - 提取通用函数 keyByField 和 sliceField2Map 减少重复代码
    - 重命名验证函数从 validateKeyByField 为 validateStructFieldKind
    - 统一 getStructFieldValue 函数参数类型为 any
    - 添加 .sisyphus/ 目录到 .gitignore 文件中

--- candy/key_by.go
+++ candy/key_by.go
@@ -5,8 +5,7 @@ import (
 	"reflect"
 )
 
-// validateKeyByField 验证KeyBy函数的字段
-func validateKeyByField(elemType reflect.Type, fieldName string, expectedKind reflect.Kind) reflect.StructField {
+func validateStructFieldKind(elemType reflect.Type, fieldName string, expectedKind reflect.Kind) {
 	// 解指针类型
 	for elemType.Kind() == reflect.Ptr {
 		elemType = elemType.Elem()
@@ -27,12 +26,10 @@ func validateKeyByField(elemType reflect.Type, fieldName string, expectedKind re
 	if field.Type.Kind() != expectedKind {
 		panic(fmt.Sprintf("field %s is not of type %s", fieldName, expectedKind))
 	}
-
-	return field
 }
 
 // getStructFieldValue 获取结构体字段的值
-func getStructFieldValue(item interface{}, fieldName string) reflect.Value {
+func getStructFieldValue(item any, fieldName string) reflect.Value {
 	v := reflect.ValueOf(item)
 	for v.Kind() == reflect.Ptr {
 		v = v.Elem()
@@ -42,324 +39,130 @@ func getStructFieldValue(item interface{}, fieldName string) reflect.Value {
 	return fieldValue
 }
 
-// KeyByInt 将结构体切片按照指定int字段的值作为key重新组织为map
-// 如果字段不存在或不是int类型，会panic
-func KeyByInt[T any](ss []T, fieldName string) map[int]T {
+func keyByField[T any, K comparable](ss []T, fieldName string, expectedKind reflect.Kind, converter func(reflect.Value) K) map[K]T {
 	if len(ss) == 0 {
 		return nil
 	}
 
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Int)
+	validateStructFieldKind(reflect.TypeOf(ss[0]), fieldName, expectedKind)
 
-	// 创建结果map
-	ret := make(map[int]T, len(ss))
-
-	// 遍历切片，构建map
+	ret := make(map[K]T, len(ss))
 	for _, item := range ss {
 		fieldValue := getStructFieldValue(item, fieldName)
-		key := int(fieldValue.Int())
-		ret[key] = item
+		ret[converter(fieldValue)] = item
 	}
 
 	return ret
+}
+
+// KeyByInt 将结构体切片按照指定int字段的值作为key重新组织为map
+// 如果字段不存在或不是int类型，会panic
+func KeyByInt[T any](ss []T, fieldName string) map[int]T {
+	return keyByField(ss, fieldName, reflect.Int, func(fieldValue reflect.Value) int {
+		return int(fieldValue.Int())
+	})
 }
 
 // KeyByInt8 将结构体切片按照指定int8字段的值作为key重新组织为map
 // 如果字段不存在或不是int8类型，会panic
 func KeyByInt8[T any](ss []T, fieldName string) map[int8]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Int8)
-
-	// 创建结果map
-	ret := make(map[int8]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := int8(fieldValue.Int())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Int8, func(fieldValue reflect.Value) int8 {
+		return int8(fieldValue.Int())
+	})
 }
 
 // KeyByInt16 将结构体切片按照指定int16字段的值作为key重新组织为map
 // 如果字段不存在或不是int16类型，会panic
 func KeyByInt16[T any](ss []T, fieldName string) map[int16]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Int16)
-
-	// 创建结果map
-	ret := make(map[int16]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := int16(fieldValue.Int())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Int16, func(fieldValue reflect.Value) int16 {
+		return int16(fieldValue.Int())
+	})
 }
 
 // KeyByInt32 将结构体切片按照指定int32字段的值作为key重新组织为map
 // 如果字段不存在或不是int32类型，会panic
 func KeyByInt32[T any](ss []T, fieldName string) map[int32]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Int32)
-
-	// 创建结果map
-	ret := make(map[int32]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := int32(fieldValue.Int())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Int32, func(fieldValue reflect.Value) int32 {
+		return int32(fieldValue.Int())
+	})
 }
 
 // KeyByInt64 将结构体切片按照指定int64字段的值作为key重新组织为map
 // 如果字段不存在或不是int64类型，会panic
 func KeyByInt64[T any](ss []T, fieldName string) map[int64]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Int64)
-
-	// 创建结果map
-	ret := make(map[int64]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := fieldValue.Int()
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Int64, func(fieldValue reflect.Value) int64 {
+		return fieldValue.Int()
+	})
 }
 
 // KeyByUint8 将结构体切片按照指定uint8字段的值作为key重新组织为map
 // 如果字段不存在或不是uint8类型，会panic
 func KeyByUint8[T any](ss []T, fieldName string) map[uint8]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Uint8)
-
-	// 创建结果map
-	ret := make(map[uint8]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := uint8(fieldValue.Uint())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Uint8, func(fieldValue reflect.Value) uint8 {
+		return uint8(fieldValue.Uint())
+	})
 }
 
 // KeyByUint16 将结构体切片按照指定uint16字段的值作为key重新组织为map
 // 如果字段不存在或不是uint16类型，会panic
 func KeyByUint16[T any](ss []T, fieldName string) map[uint16]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Uint16)
-
-	// 创建结果map
-	ret := make(map[uint16]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := uint16(fieldValue.Uint())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Uint16, func(fieldValue reflect.Value) uint16 {
+		return uint16(fieldValue.Uint())
+	})
 }
 
 // KeyByUint32 将结构体切片按照指定uint32字段的值作为key重新组织为map
 // 如果字段不存在或不是uint32类型，会panic
 func KeyByUint32[T any](ss []T, fieldName string) map[uint32]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Uint32)
-
-	// 创建结果map
-	ret := make(map[uint32]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := uint32(fieldValue.Uint())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Uint32, func(fieldValue reflect.Value) uint32 {
+		return uint32(fieldValue.Uint())
+	})
 }
 
 // KeyByUint64 将结构体切片按照指定uint64字段的值作为key重新组织为map
 // 如果字段不存在或不是uint64类型，会panic
 func KeyByUint64[T any](ss []T, fieldName string) map[uint64]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Uint64)
-
-	// 创建结果map
-	ret := make(map[uint64]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := fieldValue.Uint()
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Uint64, func(fieldValue reflect.Value) uint64 {
+		return fieldValue.Uint()
+	})
 }
 
 // KeyByUint 将结构体切片按照指定uint字段的值作为key重新组织为map
 // 如果字段不存在或不是uint类型，会panic
 func KeyByUint[T any](ss []T, fieldName string) map[uint]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Uint)
-
-	// 创建结果map
-	ret := make(map[uint]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := uint(fieldValue.Uint())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Uint, func(fieldValue reflect.Value) uint {
+		return uint(fieldValue.Uint())
+	})
 }
 
 // KeyByFloat32 将结构体切片按照指定float32字段的值作为key重新组织为map
 // 如果字段不存在或不是float32类型，会panic
 func KeyByFloat32[T any](ss []T, fieldName string) map[float32]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Float32)
-
-	// 创建结果map
-	ret := make(map[float32]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := float32(fieldValue.Float())
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Float32, func(fieldValue reflect.Value) float32 {
+		return float32(fieldValue.Float())
+	})
 }
 
 // KeyByFloat64 将结构体切片按照指定float64字段的值作为key重新组织为map
 // 如果字段不存在或不是float64类型，会panic
 func KeyByFloat64[T any](ss []T, fieldName string) map[float64]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Float64)
-
-	// 创建结果map
-	ret := make(map[float64]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := fieldValue.Float()
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Float64, func(fieldValue reflect.Value) float64 {
+		return fieldValue.Float()
+	})
 }
 
 // KeyByString 将结构体切片按照指定string字段的值作为key重新组织为map
 // 如果字段不存在或不是string类型，会panic
 func KeyByString[T any](ss []T, fieldName string) map[string]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.String)
-
-	// 创建结果map
-	ret := make(map[string]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := fieldValue.String()
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.String, func(fieldValue reflect.Value) string {
+		return fieldValue.String()
+	})
 }
 
 // KeyByBool 将结构体切片按照指定bool字段的值作为key重新组织为map
 // 如果字段不存在或不是bool类型，会panic
 func KeyByBool[T any](ss []T, fieldName string) map[bool]T {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// 验证字段
-	validateKeyByField(reflect.TypeOf(ss[0]), fieldName, reflect.Bool)
-
-	// 创建结果map
-	ret := make(map[bool]T, len(ss))
-
-	// 遍历切片，构建map
-	for _, item := range ss {
-		fieldValue := getStructFieldValue(item, fieldName)
-		key := fieldValue.Bool()
-		ret[key] = item
-	}
-
-	return ret
+	return keyByField(ss, fieldName, reflect.Bool, func(fieldValue reflect.Value) bool {
+		return fieldValue.Bool()
+	})
 }
--- candy/slice_to_map.go
+++ candy/slice_to_map.go
@@ -1,7 +1,6 @@
 package candy
 
 import (
-	"fmt"
 	"reflect"
 
 	"golang.org/x/exp/constraints"
@@ -18,359 +17,130 @@ func Slice2Map[M constraints.Ordered](list []M) map[M]bool {
 	return m
 }
 
-// validateSliceFieldForMap validates the field for SliceField2Map functions
-func validateSliceFieldForMap(elemType reflect.Type, fieldName string, expectedKind reflect.Kind) {
-	// Dereference pointer types
-	for elemType.Kind() == reflect.Ptr {
-		elemType = elemType.Elem()
+func sliceField2Map[T any, K comparable](ss []T, fieldName string, expectedKind reflect.Kind, converter func(reflect.Value) K) map[K]bool {
+	if len(ss) == 0 {
+		return nil
 	}
 
-	// Check if it's a struct
-	if elemType.Kind() != reflect.Struct {
-		panic("element is not a struct")
+	validateStructFieldKind(reflect.TypeOf(ss[0]), fieldName, expectedKind)
+
+	ret := make(map[K]bool, len(ss))
+	for _, item := range ss {
+		fieldValue := getStructFieldValue(item, fieldName)
+		ret[converter(fieldValue)] = true
 	}
 
-	// Find the field
-	field, ok := elemType.FieldByName(fieldName)
-	if !ok {
-		panic(fmt.Sprintf("field %s not found", fieldName))
-	}
-
-	// Check field type
-	if field.Type.Kind() != expectedKind {
-		panic(fmt.Sprintf("field %s is not of type %s", fieldName, expectedKind))
-	}
-}
-
-// getSliceFieldValue gets the value of a struct field
-func getSliceFieldValue(item interface{}, fieldName string) reflect.Value {
-	v := reflect.ValueOf(item)
-	for v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-
-	fieldValue := v.FieldByName(fieldName)
-	return fieldValue
+	return ret
 }
 
 // SliceField2MapString extracts string field values from a struct slice and creates a map[string]bool
 // Panics if the field doesn't exist or is not a string type
 func SliceField2MapString[T any](ss []T, fieldName string) map[string]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.String)
-
-	// Create result map
-	ret := make(map[string]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := fieldValue.String()
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.String, func(fieldValue reflect.Value) string {
+		return fieldValue.String()
+	})
 }
 
 // SliceField2MapInt extracts int field values from a struct slice and creates a map[int]bool
 // Panics if the field doesn't exist or is not an int type
 func SliceField2MapInt[T any](ss []T, fieldName string) map[int]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Int)
-
-	// Create result map
-	ret := make(map[int]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := int(fieldValue.Int())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Int, func(fieldValue reflect.Value) int {
+		return int(fieldValue.Int())
+	})
 }
 
 // SliceField2MapInt8 extracts int8 field values from a struct slice and creates a map[int8]bool
 // Panics if the field doesn't exist or is not an int8 type
 func SliceField2MapInt8[T any](ss []T, fieldName string) map[int8]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Int8)
-
-	// Create result map
-	ret := make(map[int8]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := int8(fieldValue.Int())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Int8, func(fieldValue reflect.Value) int8 {
+		return int8(fieldValue.Int())
+	})
 }
 
 // SliceField2MapInt16 extracts int16 field values from a struct slice and creates a map[int16]bool
 // Panics if the field doesn't exist or is not an int16 type
 func SliceField2MapInt16[T any](ss []T, fieldName string) map[int16]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Int16)
-
-	// Create result map
-	ret := make(map[int16]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := int16(fieldValue.Int())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Int16, func(fieldValue reflect.Value) int16 {
+		return int16(fieldValue.Int())
+	})
 }
 
 // SliceField2MapInt32 extracts int32 field values from a struct slice and creates a map[int32]bool
 // Panics if the field doesn't exist or is not an int32 type
 func SliceField2MapInt32[T any](ss []T, fieldName string) map[int32]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Int32)
-
-	// Create result map
-	ret := make(map[int32]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := int32(fieldValue.Int())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Int32, func(fieldValue reflect.Value) int32 {
+		return int32(fieldValue.Int())
+	})
 }
 
 // SliceField2MapInt64 extracts int64 field values from a struct slice and creates a map[int64]bool
 // Panics if the field doesn't exist or is not an int64 type
 func SliceField2MapInt64[T any](ss []T, fieldName string) map[int64]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Int64)
-
-	// Create result map
-	ret := make(map[int64]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := fieldValue.Int()
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Int64, func(fieldValue reflect.Value) int64 {
+		return fieldValue.Int()
+	})
 }
 
 // SliceField2MapUint extracts uint field values from a struct slice and creates a map[uint]bool
 // Panics if the field doesn't exist or is not a uint type
 func SliceField2MapUint[T any](ss []T, fieldName string) map[uint]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Uint)
-
-	// Create result map
-	ret := make(map[uint]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := uint(fieldValue.Uint())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Uint, func(fieldValue reflect.Value) uint {
+		return uint(fieldValue.Uint())
+	})
 }
 
 // SliceField2MapUint8 extracts uint8 field values from a struct slice and creates a map[uint8]bool
 // Panics if the field doesn't exist or is not a uint8 type
 func SliceField2MapUint8[T any](ss []T, fieldName string) map[uint8]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Uint8)
-
-	// Create result map
-	ret := make(map[uint8]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := uint8(fieldValue.Uint())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Uint8, func(fieldValue reflect.Value) uint8 {
+		return uint8(fieldValue.Uint())
+	})
 }
 
 // SliceField2MapUint16 extracts uint16 field values from a struct slice and creates a map[uint16]bool
 // Panics if the field doesn't exist or is not a uint16 type
 func SliceField2MapUint16[T any](ss []T, fieldName string) map[uint16]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Uint16)
-
-	// Create result map
-	ret := make(map[uint16]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := uint16(fieldValue.Uint())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Uint16, func(fieldValue reflect.Value) uint16 {
+		return uint16(fieldValue.Uint())
+	})
 }
 
 // SliceField2MapUint32 extracts uint32 field values from a struct slice and creates a map[uint32]bool
 // Panics if the field doesn't exist or is not a uint32 type
 func SliceField2MapUint32[T any](ss []T, fieldName string) map[uint32]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Uint32)
-
-	// Create result map
-	ret := make(map[uint32]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := uint32(fieldValue.Uint())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Uint32, func(fieldValue reflect.Value) uint32 {
+		return uint32(fieldValue.Uint())
+	})
 }
 
 // SliceField2MapUint64 extracts uint64 field values from a struct slice and creates a map[uint64]bool
 // Panics if the field doesn't exist or is not a uint64 type
 func SliceField2MapUint64[T any](ss []T, fieldName string) map[uint64]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Uint64)
-
-	// Create result map
-	ret := make(map[uint64]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := fieldValue.Uint()
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Uint64, func(fieldValue reflect.Value) uint64 {
+		return fieldValue.Uint()
+	})
 }
 
 // SliceField2MapFloat32 extracts float32 field values from a struct slice and creates a map[float32]bool
 // Panics if the field doesn't exist or is not a float32 type
 func SliceField2MapFloat32[T any](ss []T, fieldName string) map[float32]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Float32)
-
-	// Create result map
-	ret := make(map[float32]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := float32(fieldValue.Float())
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Float32, func(fieldValue reflect.Value) float32 {
+		return float32(fieldValue.Float())
+	})
 }
 
 // SliceField2MapFloat64 extracts float64 field values from a struct slice and creates a map[float64]bool
 // Panics if the field doesn't exist or is not a float64 type
 func SliceField2MapFloat64[T any](ss []T, fieldName string) map[float64]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Float64)
-
-	// Create result map
-	ret := make(map[float64]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := fieldValue.Float()
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Float64, func(fieldValue reflect.Value) float64 {
+		return fieldValue.Float()
+	})
 }
 
 // SliceField2MapBool extracts bool field values from a struct slice and creates a map[bool]bool
 // Panics if the field doesn't exist or is not a bool type
 func SliceField2MapBool[T any](ss []T, fieldName string) map[bool]bool {
-	if len(ss) == 0 {
-		return nil
-	}
-
-	// Validate field
-	validateSliceFieldForMap(reflect.TypeOf(ss[0]), fieldName, reflect.Bool)
-
-	// Create result map
-	ret := make(map[bool]bool, len(ss))
-
-	// Iterate through slice and build map
-	for _, item := range ss {
-		fieldValue := getSliceFieldValue(item, fieldName)
-		key := fieldValue.Bool()
-		ret[key] = true
-	}
-
-	return ret
+	return sliceField2Map(ss, fieldName, reflect.Bool, func(fieldValue reflect.Value) bool {
+		return fieldValue.Bool()
+	})
 }
